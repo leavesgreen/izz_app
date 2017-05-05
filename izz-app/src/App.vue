9a32ec362e1ccd081b8c71580520e9555e77579b
guoqiang.jia@zhenip.com <guoqiang.jia@zhenip.com>
2017-05-05 20:37:46 +0800

    begin the izz app dev

---
+++ izz-app/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app2">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'app2'
+}
+
+</script>
+
+<style>
+#app2 {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
---
+++ izz-app/src/components/PicsUp.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="picsup">
+    <mt-header :title="vtitle">
+      <router-link to="/" slot="left">
+        <mt-button icon="back">返回</mt-button>
+      </router-link>
+      <mt-button icon="more" slot="right"></mt-button>
+    </mt-header>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue'
+import { Header } from 'mint-ui'
+
+Vue.component(Header.name, Header)
+
+export default {
+  name: 'picsup',
+  data () {
+    return {
+      vtitle: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
